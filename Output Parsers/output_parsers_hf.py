b80cee53edf0fe798e805cb291ef3c33bf6c06b4
Sapy14 <sapy.sharma14@gmail.com>
2026-05-31 21:50:27 +0200

    json output

--- Output Parsers/output_parsers_hf.py
+++ Output Parsers/output_parsers_hf.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
@@ -14,8 +15,18 @@ model=ChatHuggingFace(llm=llm)
 template1=PromptTemplate(
     template='Write a detailed report on {topic}',
     input_variables=['topic']
+    
 )
 
-prompt1=template1.invoke({'topic':'LLM'})
-result=model.invoke(prompt1)
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
 print(result.content)
